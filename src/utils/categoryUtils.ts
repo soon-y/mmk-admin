c76a32ccd304f78672ce21681d1dff71b67cce27
Soonyoung Park <soon.syp@gmail.com>
2025-07-06 23:47:33 +0200

    update

--- src/utils/categoryUtils.ts
+++ src/utils/categoryUtils.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import type { CategoryProps, GroupedCategory } from "../types"
 
 export const sortData = (categories: CategoryProps[]) => {
@@ -13,6 +15,7 @@ export const sortData = (categories: CategoryProps[]) => {
     id: group.id,
     name: group.name,
     order: group.order,
+    image: group.image,
     children: items.filter((item) => item.groupID === group.id),
   }))
 
@@ -27,5 +30,15 @@ export const fetchCategory = async () => {
   } catch (err) {
     console.error('Failed to fetch products:', err)
     return err
+  }
+}
+
+export const updateCategory = async (data: FormData) => {
+  try {
+    const res = await axios.post('http://localhost:3000/category/replace', data)
+    return res
+  } catch (err: unknown) {
+    const error = err as any
+    console.error(error.response?.data || error.message || error)
   }
 }
--- src/utils/productUtils.ts
+++ src/utils/productUtils.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const fetchProducts = async () => {
   try {
     const res = await fetch('https://mmk-backend.onrender.com/products')
@@ -5,5 +7,55 @@ export const fetchProducts = async () => {
     return data
   } catch (err) {
     console.error('Failed to fetch products:', err)
+  }
+}
+
+export const groupingImages = (existingImagesCount: string[], images: string[]) => {
+  const groupedImages: string[][] = []
+  let start = 0
+
+  for (const count of existingImagesCount) {
+    groupedImages.push(images.slice(start, start + Number(count)))
+    start += Number(count)
+  }
+
+  return groupedImages
+}
+
+export const postProducts = async (formData: FormData) => {
+  try {
+    const res = await axios.post('http://localhost:3000/products', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    return res
+  } catch (err: unknown) {
+    const error = err as any
+    console.error(error.response?.data || error.message || error)
+  }
+}
+
+export const updateProduct = async (id: number, formData: FormData) => {
+  try {
+    const res = await axios.post(`http://localhost:3000/products/${id}`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    return res
+  } catch (err: unknown) {
+    const error = err as any
+    console.error(error.response?.data || error.message || error)
+  }
+}
+
+export const deleteProduct  = async (id: number) => {
+  try {
+    const res = await axios.delete(`https://mmk-backend.onrender.com/products/${id}`)
+    return res
+  } catch (err: unknown) {
+    const error = err as any
+    console.error(error.response?.data || error.message || error)
   }
 }
