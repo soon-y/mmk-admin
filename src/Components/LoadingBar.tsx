be5921024f27b8ac8d172b3e8112baf449f872bf
Soonyoung Park <soon.syp@gmail.com>
2025-07-06 23:41:16 +0200

    update loading img

--- src/Components/LoadingBar.tsx
+++ src/Components/LoadingBar.tsx
@@ -1,11 +1,9 @@
-import { Loader } from "lucide-react"
+import LoadingIcon from "../asset/LoadingIcon"
 
 export default function LoadingBar() {
-
-
   return (
-    <div className="container fixed top-0 left-0 w-full h-full backdrop-blur-xs flex items-center justify-center">
-      <Loader className="animate-spin" />
+    <div className="container fixed top-0 left-0 w-full h-full backdrop-blur-xs flex items-center justify-center bg-white/60">
+      <LoadingIcon />
     </div>
   )
 }
