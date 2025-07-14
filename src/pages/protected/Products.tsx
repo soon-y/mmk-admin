6fd0d7efc64e405a0b23f9efa43d7a105c6569ee
Soonyoung Park <soon.syp@gmail.com>
2025-07-14 15:39:41 +0200

    add styles for container for wide layout

--- src/pages/protected/Products.tsx
+++ src/pages/protected/Products.tsx
@@ -19,7 +19,7 @@ function Products() {
   const [sizeFilterData, setSizeFilterData] = useState<string[]>([])
   const [colorFilterData, setColorFilterData] = useState<string[]>([])
   const [category, setCategory] = useState<CategoryProps[]>([])
-  const style = 'grid grid-cols-[70px_100px_1fr_80px_90px_120px_80px_120px] gap-2 items-center'
+  const style = 'grid grid-cols-[70px_100px_150px_80px_90px_120px_80px_120px] lg:grid-cols-[70px_100px_1fr_80px_90px_120px_80px_120px] gap-2 items-center'
 
   useEffect(() => {
     Promise.all([fetchCategory(), fetchProducts()])
@@ -97,64 +97,65 @@ function Products() {
     (!sizeFilter || p.size === sizeFilter) &&
     (!colorFilter || p.color === colorFilter)
   ).sort((a, b) => {
-      if (!sortBy) return 0
-      const valA = a[sortBy]
-      const valB = b[sortBy]
+    if (!sortBy) return 0
+    const valA = a[sortBy]
+    const valB = b[sortBy]
 
-      if (typeof valA === 'number' && typeof valB === 'number') {
-        return sortDir === 'asc' ? valA - valB : valB - valA
-      }
+    if (typeof valA === 'number' && typeof valB === 'number') {
+      return sortDir === 'asc' ? valA - valB : valB - valA
+    }
 
-      return sortDir === 'asc'
-        ? String(valA).localeCompare(String(valB))
-        : String(valB).localeCompare(String(valA))
-    })
+    return sortDir === 'asc'
+      ? String(valA).localeCompare(String(valB))
+      : String(valB).localeCompare(String(valA))
+  })
 
   return (
-    <div className="container p-8">
-      <div className={`${style} h-12 bg-white rounded-xl font-bold text-sm mb-2`}>
-        <div className='cursor-pointer h-5 flex itmes-center ml-4' onClick={() => toggleSort('id')}>ID <SortArrow field="id" /> </div>
-        <CategorySelection disabled={false} option='Category' categoryIndex={categoryFilter} noPadding={true} setValue={setCategoryFilter} setLoading={setLoading} />
-        <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('name')}>Name <SortArrow field="name" /></div>
-        <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('price')}>Price <SortArrow field="price" /></div>
-        <Selection value={sizeFilter} setValue={setSizeFilter} option='Size' data={sizeFilterData} />
-        <Selection value={colorFilter} setValue={setColorFilter} option='Color' data={colorFilterData} />
-        <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('stock')}>Stock <SortArrow field="stock" /></div>
-        <p>IMG</p>
+    <div className="container-overflow-x">
+      <div className="p-8 w-max">
+        <div className={`${style} h-12 bg-white rounded-xl font-bold text-sm mb-2`}>
+          <div className='cursor-pointer h-5 flex itmes-center ml-4' onClick={() => toggleSort('id')}>ID <SortArrow field="id" /> </div>
+          <CategorySelection disabled={false} option='Category' categoryIndex={categoryFilter} noPadding={true} setValue={setCategoryFilter} setLoading={setLoading} />
+          <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('name')}>Name <SortArrow field="name" /></div>
+          <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('price')}>Price <SortArrow field="price" /></div>
+          <Selection value={sizeFilter} setValue={setSizeFilter} option='Size' data={sizeFilterData} />
+          <Selection value={colorFilter} setValue={setColorFilter} option='Color' data={colorFilterData} />
+          <div className='cursor-pointer h-5 flex itmes-center' onClick={() => toggleSort('stock')}>Stock <SortArrow field="stock" /></div>
+          <p>IMG</p>
+        </div>
+
+        {sortedProducts.map((product, index) => (
+          <div onClick={() => navigate(`/product/${product.id}`)} key={index}
+            className={`${style} border-b border-gray-300 cursor-pointer`}>
+            <p className='ml-4'>{product.id}</p>
+            <p>{category[product.category - 1].name}</p>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+            <p>{product.size}</p>
+            <p>{product.color}</p>
+            <p>{product.stock}</p>
+            <img src={product.images[0]} alt="Product" />
+          </div>
+        ))}
+
+        {loading && Array.from({ length: 10 }).map((_, i) => (
+          <div key={i} className={`${style} border-b border-gray-300 animate-pulse`}>
+            <p className='ml-4 rounded-md w-8 h-6 bg-gray-200'></p>
+            <p className='rounded-md w-[90%] h-6 bg-gray-200'></p>
+            <p className='rounded-md w-[50%] h-6 bg-gray-200'></p>
+            <p className='rounded-md w-6 h-6 bg-gray-200'></p>
+            <p className='rounded-md w-18 h-6 bg-gray-200'></p>
+            <div className='flex gap-2'>
+              <p key={i} className='border border-gray-300 rounded-full w-5 aspect-square bg-gray-200'></p>
+              <p className='rounded-md w-12 h-6 bg-gray-200'></p>
+            </div>
+            <p className='rounded-md w-6 h-6 bg-gray-200'></p>
+            <div className='w-[120px] aspect-square bg-gray-200 grid place-items-center'>
+              <Image className='text-gray-400' />
+            </div>
+          </div>
+        ))}
       </div>
-
-      {sortedProducts.map((product, index) => (
-        <div onClick={() => navigate(`/product/${product.id}`)} key={index}
-          className={`${style} border-b border-gray-300 cursor-pointer`}>
-          <p className='ml-4'>{product.id}</p>
-          <p>{category[product.category - 1].name}</p>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
-          <p>{product.size}</p>
-          <p>{product.color}</p>
-          <p>{product.stock}</p>
-          <img src={product.images[0]} alt="Product" />
-        </div>
-      ))}
-
-      {loading && Array.from({ length: 10 }).map((_, i) => (
-        <div key={i} className={`${style} border-b border-gray-300 animate-pulse`}>
-          <p className='ml-4 rounded-md w-8 h-6 bg-gray-200'></p>
-          <p className='rounded-md w-[90%] h-6 bg-gray-200'></p>
-          <p className='rounded-md w-[50%] h-6 bg-gray-200'></p>
-          <p className='rounded-md w-6 h-6 bg-gray-200'></p>
-          <p className='rounded-md w-18 h-6 bg-gray-200'></p>
-          <div className='flex gap-2'>
-            <p key={i} className='border border-gray-300 rounded-full w-5 aspect-square bg-gray-200'></p>
-            <p className='rounded-md w-12 h-6 bg-gray-200'></p>
-          </div>
-          <p className='rounded-md w-6 h-6 bg-gray-200'></p>
-          <div className='w-[120px] aspect-square bg-gray-200 grid place-items-center'>
-            <Image className='text-gray-400' />
-          </div>
-        </div>
-      ))}
-
     </div>
   )
 }
