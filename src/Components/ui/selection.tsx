e081c202dd4d3dc2c0724db2f97d6631371e766b
Soonyoung Park <soon.syp@gmail.com>
2025-07-30 17:01:17 +0200

    update param option as optional

--- src/Components/ui/selection.tsx
+++ src/Components/ui/selection.tsx
@@ -3,7 +3,7 @@ export default function Selection(
     value, option, setValue, data
   }: {
     value: string,
-    option: string
+    option?: string
     data: string[],
     setValue: React.Dispatch<React.SetStateAction<string>>
   }) {
@@ -14,7 +14,7 @@ export default function Selection(
       onChange={(e) => setValue(e.target.value)}
       className="cursor-pointer bg-transparent focus:outline-none"
     >
-      <option value="">{option}</option>
+      {option && <option value="">{option}</option>}
       {data.map((el, i) => (
         <option value={el} key={i}>
           {el}
