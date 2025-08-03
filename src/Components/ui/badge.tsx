8dc2545c19961b1b6f532ac1619b61bdcac324f6
Soonyoung Park <soon.syp@gmail.com>
2025-08-03 21:51:31 +0200

    update styles

--- src/Components/ui/badge.tsx
+++ src/Components/ui/badge.tsx
@@ -2,11 +2,11 @@ export default function Badge(status: string) {
   return (
     <div>
       <div className={`
-      ${status === 'approved' || status === 'delivered' ? 'bg-green-100 border-green-600 text-green-600' : ''} 
-      ${status === 'declined' ? 'bg-red-100 border-red-500 text-red-500' : ''} 
-      ${status === 'ordered' ? 'bg-gray-100 border-black text-black' : ''} 
+      ${status === 'approved' || status === 'delivered' ? 'bg-green-50 border-green-600 text-green-600' : ''} 
+      ${status === 'declined' ? 'bg-red-50 border-red-500 text-red-500' : ''} 
+      ${status === 'ordered' ? 'bg-gray-50 border-black text-black' : ''} 
       ${status === 'processing completed' ? 'bg-yellow-100 border-yellow-600 text-yellow-600' : ''} 
-      ${status === 'shipped' ? 'bg-orange-100 border-orange-600 text-orange-600' : ''} 
+      ${status === 'shipped' ? 'bg-orange-50 border-orange-600 text-orange-600' : ''} 
       flex justify-center border-2 py-1 px-2 rounded-full w-fit`}>
         <p className="capitalize">{status}</p>
       </div>
