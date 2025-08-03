55959b30f846b294fde7548923d40359f611338e
Soonyoung Park <soon.syp@gmail.com>
2025-08-03 21:49:54 +0200

    add new color

--- src/Components/Palette.tsx
+++ src/Components/Palette.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import MMKColor from "../asset/mmk"
 
 export function Palette({ initial, index, setColorArray, setColorHex }: {
   index: number,
@@ -6,8 +7,8 @@ export function Palette({ initial, index, setColorArray, setColorHex }: {
   setColorArray: React.Dispatch<React.SetStateAction<string[]>>,
   setColorHex: React.Dispatch<React.SetStateAction<string[]>>
 }) {
-  const palette: string[] = ['#ffffff', '#000000', '#e3007f', '#00a0e8', '#ffef00']
-  const colors: string[] = ['White', 'Black', 'Magenta', 'Blue', 'Yellow']
+  const palette: string[] = ['#ffffff', '#000000', '#e3007f', '#00a0e8', '#ffef00', 'MMK']
+  const colors: string[] = ['White', 'Black', 'Magenta', 'Blue', 'Yellow', 'Mixed']
   const [colorIndex, setColorIndex] = useState<number>(palette.indexOf(initial))
 
   useEffect(() => {
@@ -27,6 +28,9 @@ export function Palette({ initial, index, setColorArray, setColorHex }: {
       <div onClick={() => setColorIndex(2)} className={`${colorIndex === 2 ? 'border-gray-700' : 'border-gray-300 opacity-50'} rounded-full border border-2 w-5 h-5 bg-[#e3007f]`}></div>
       <div onClick={() => setColorIndex(3)} className={`${colorIndex === 3 ? 'border-gray-700' : 'border-gray-300 opacity-50'} rounded-full border border-2 w-5 h-5 bg-[#00a0e8]`}></div>
       <div onClick={() => setColorIndex(4)} className={`${colorIndex === 4 ? 'border-gray-700' : 'border-gray-300 opacity-50'} rounded-full border border-2 w-5 h-5 bg-[#ffef00]`}></div>
+      <div onClick={() => setColorIndex(5)} className={`${colorIndex === 5 ? 'border-gray-700' : 'border-gray-300 opacity-50'} rounded-full border border-2 w-5 h-5 overflow-hidden bg-white"`}>
+        <MMKColor />
+      </div>
     </div>
   )
 }
