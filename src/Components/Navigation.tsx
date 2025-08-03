9a4aad35c2627b2d0ac6f321c569560cd01114ec
Soonyoung Park <soon.syp@gmail.com>
2025-08-03 21:50:16 +0200

    update label name

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -11,7 +11,7 @@ function Navigation() {
     { link: 'dashboard', label: 'Dashboard', icon: <LayoutDashboard strokeWidth={1} /> },
     { link: 'products', label: 'Products', icon: <Boxes strokeWidth={1} /> },
     { link: 'new-product', label: 'New Product', icon: <PackagePlus strokeWidth={1} /> },
-    { link: 'orders', label: 'Order', icon: <Truck strokeWidth={1} /> },
+    { link: 'orders', label: 'Orders', icon: <Truck strokeWidth={1} /> },
     { link: 'customers', label: 'Customers', icon: <CircleUserRound strokeWidth={1} /> },
     { link: 'settings', label: 'Settings', icon: <Settings strokeWidth={1} /> },
     { link: 'profile', label: 'Profile', icon: user ? <img className="w-6 rounded-full" src={user.avatar_url} /> : <div className="w-6 aspect-square bg-gray-200 rounded-full animate-pulse"></div> },
